738c46f20de7e64d0c5391d4a57a4b74b5f07c7d
Julia Jaca <jjaca@cifpfbmoll.eu>
2021-04-28 19:23:00 +0200

    Crear clase obtener conexion en el main e instanciar un objeto miConexion

--- src/main/java/Universidad/Main.java
+++ src/main/java/Universidad/Main.java
@@ -1,4 +1,19 @@
 package Universidad;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+     
+           Connection miConexion = obtenerConexion();
+    }
+
+
+    public static Connection obtenerConexion () throws SQLException {
+        String url = "jdbc:mysql://51.178.152.223:3306/dam4";
+        return DriverManager.getConnection (url, "Dam4", "ProyectoGrupo4");
+    }
 }
