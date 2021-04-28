9d827324ddb5f7ec3f9b826b8ee3973e9045fbb5
Alberto <aserranor@cifpfbmoll.eu>
2021-04-28 19:27:56 +0200

    Crear clase Administrador
    He creado la clase Administrador, los atributos, los 3 constructores y el getter y el setter

---
+++ src/main/java/Universidad/Administrador.java
@@ -0,0 +1,34 @@
+package Universidad;
+
+public class Administrador extends Persona{
+    
+    // ATRIBUTOS
+
+    private String ID_Persona;
+
+    // CONSTRUCTOR VACÍO, CONSTRUCTOR CON PARÁMETROS Y CONSTRUCTOR COPIA
+
+    public Administrador() {
+    }
+
+    public Administrador(String ID_Persona, String nombre, int edad, String telefono, String contraseña, int ID_Persona1) {
+        super(ID_Persona, nombre, edad, telefono, contraseña);
+        this.setID_Persona(ID_Persona);
+    }
+
+    public Administrador(Administrador copiaAdministrador){
+        super((Persona)copiaAdministrador);
+        this.setID_Persona(copiaAdministrador.getID_Persona());
+    }
+
+    // GETTERS & SETTERS
+
+    @Override
+    public String getID_Persona() {
+        return ID_Persona;
+    }
+
+    public void setID_Persona(String ID_Persona) {
+        this.ID_Persona = ID_Persona;
+    }
+}
