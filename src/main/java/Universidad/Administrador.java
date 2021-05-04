6164c6146bf54d0e44f2891ca34d7d9fe9fd924b
Julia Jaca <jjaca@cifpfbmoll.eu>
2021-05-04 17:26:55 +0200

    añadir persona WIP

--- src/main/java/Universidad/Administrador.java
+++ src/main/java/Universidad/Administrador.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Administrador extends Persona{
     
@@ -47,6 +48,40 @@ public class Administrador extends Persona{
 
 
     }
+
+    public static Sring[] pedirDatosPersona(){
+        Scanner lector = new  Scanner(System.in);
+        String tipoPersona;
+        do{
+            System.out.println("¿Qué tipo de persona quieres introducir?");
+            System.out.println("1. Administrador");
+            System.out.println("2. Profesor");
+            System.out.println("3. Alumno");
+            System.out.println("4. Bibliotecario");
+            tipoPersona = lector.nextLine();
+
+        }while (tipoPersona != "1" || tipoPersona != "2" || tipoPersona != "2" || tipoPersona != "2");
+        System.out.println("Dame el DNI");
+        String DNI = lector.nextLine();
+        System.out.println("Dame la contrasena");
+        String constrasena = lector.nextLine();
+        System.out.println("Dime el telefono");
+        String telefono = lector.nextLine();
+        System.out.println("Dime la edad");
+        int edad = lector.nextInt();
+        lector.nextLine();
+
+
+
+
+    }
+    public static void anadirPersona(Connection miConexion){
+        Scanner lector = new  Scanner(System.in);
+        System.out.println("¿Qué persona quieres introducir?");
+        System.out.println();
+
+    }
+
 
 
 }
