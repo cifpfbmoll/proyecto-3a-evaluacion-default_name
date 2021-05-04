18d9aab9c8a5a0c922654a4999fdd1140eabed3c
cgranda3 <cgranda@cifpfbmoll.eu>
2021-05-04 16:28:46 +0200

    Arreglo de la clase profesor

--- src/main/java/Universidad/Profesor.java
+++ src/main/java/Universidad/Profesor.java
@@ -3,7 +3,6 @@ package Universidad;
 public class Profesor extends Persona{
 
     // ATRIBUTOS
-    private String ID_Persona;
     private String ID_Departamento;
 
     // CONSTRUCTOR VACÍO, CONSTRUCTOR CON PARÁMETROS, CONSTRUCTOR COPIA
@@ -13,25 +12,15 @@ public class Profesor extends Persona{
 
     public Profesor(String ID_Persona, String nombre, int edad, String telefono, String contrasena) {
         super(ID_Persona, nombre, edad, telefono, contrasena);
-        this.setID_Persona(ID_Persona);
         this.setID_Departamento(ID_Departamento);
     }
 
     public Profesor(Profesor copiaProfesor){
         super((Persona)copiaProfesor);
-        this.setID_Persona(copiaProfesor.getID_Persona());
         this.setID_Departamento(copiaProfesor.getID_Departamento());
     }
 
     // GETTERS & SETTERS
-
-    public String getID_Persona() {
-        return ID_Persona;
-    }
-
-    public void setID_Persona(String ID_Persona) {
-        this.ID_Persona = ID_Persona;
-    }
 
     public String getID_Departamento() {
         return ID_Departamento;
