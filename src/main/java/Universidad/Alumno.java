63b52ef71d405a203c19afc22920906c127a56cd
Alberto <aserranor@cifpfbmoll.eu>
2021-05-05 21:28:06 +0200

    metodosProfesor(in progress)
    mostrarAlumnos( falta el select)
    falta metodo ponerNota() y JAVADOC

--- src/main/java/Universidad/Alumno.java
+++ src/main/java/Universidad/Alumno.java
@@ -9,8 +9,8 @@ public class Alumno extends Persona{
     public Alumno(String ID_Persona, String nombre, int edad, String telefono, String contrasena) {
         super(ID_Persona, nombre, edad, telefono, contrasena);
 
-    public Alumno(Alumno copiaAlumno) {
-            super((Persona)copiaAlumno);
+    //public Alumno(Alumno copiaAlumno) {
+    //    super((Persona)copiaAlumno);
         }
 
     }
