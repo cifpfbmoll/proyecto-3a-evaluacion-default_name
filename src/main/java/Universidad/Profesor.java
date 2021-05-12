e0aebc29c5043909dc3f28ad8edef31e3b9bec2b
cgranda3 <cgranda@cifpfbmoll.eu>
2021-05-12 21:10:22 +0200

    arreglo clase profesor

--- src/main/java/Universidad/Profesor.java
+++ src/main/java/Universidad/Profesor.java
@@ -100,4 +100,64 @@ public class Profesor extends Persona{
             e.printStackTrace();
         }
     }
+
+    /**
+     * Metodo estatico que se utilizara para poner notas a los alumnos
+     * @param miConexion
+     * @param datos
+     * @throws SQLException
+     */
+    public static void ponerNota(Connection miConexion, String[] datos) throws SQLException{
+        Scanner lector = new Scanner(System.in);
+
+        mostrarAlumnos(miConexion, datos);
+        System.out.println("Escribe el ID del Alumno al que quieres ponerle nota:");
+        System.out.println("ID: ");
+        String id = lector.nextLine();
+
+        System.out.println("Que nota quieres ponerle?");
+        Double nota = lector.nextDouble();
+        lector.nextLine();
+
+        try {
+            PreparedStatement prepStat = miConexion.prepareStatement("UPDATE matriculacion" +
+                    "                                                     SET Nota=?" +
+                    "                                                     WHERE ID_Persona=?");
+
+            prepStat.setDouble(1, nota);
+            prepStat.setString(2, id);
+
+            int n = prepStat.executeUpdate();
+
+        }catch(SQLException e){
+            System.out.println("No se ha podido insertar la nota");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void mostrarAlumnos(Connection miConexion, String[] datos) throws SQLException {
+
+        System.out.println("Estos son los alumnos que tienes:");
+        try{
+
+            PreparedStatement prepStat = miConexion.prepareStatement("SELECT p.ID_Persona, p.Nombre, p.Edad, p.Telefono " +
+                    "                                                    FROM Persona AS p " +
+                    "                                                    INNER JOIN Matriculacion AS m ON m.ID_Persona = p.ID_Persona" +
+                    "                                                    INNER JOIN Asignatura AS a ON a.ID_Asignatura = m.ID_Asignatura " +
+                    "                                                    INNER JOIN Profesor AS pr ON pr.ID_Persona = a.ID_Profesor " +
+                    "                                                    WHERE pr.ID_Persona = ? ");
+            prepStat.setString(1, datos[0]);
+            ResultSet resultado = prepStat.executeQuery();
+            while(resultado.next()){
+                String trampitas = resultado.getString("ID_Persona") + " " + resultado.getString("Nombre") +
+                        " " + resultado.getString("Edad") + " " + resultado.getString("Telefono");
+                System.out.println(trampitas.replace(" ", " - "));
+            }
+        }catch(SQLException e){
+            System.out.println("No se ha podido realizar la consulta.");
+            e.printStackTrace();
+        }
+    }
+
 }
