1e887ce932bc51b14adb0e7ef9dddf3c4788b9a1
Alfons Lorente Darder <alorente@cifpfbmoll.com>
2021-05-18 16:29:56 +0200

    nueva_gestion_universidad y script
    Cambiado el script sql y la conexion a nueva_gestion_universidad

--- src/main/java/Universidad/Main.java
+++ src/main/java/Universidad/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
 
     public static Connection obtenerConexion () throws SQLException {
-        String url = "jdbc:mysql://51.178.152.223:3306/gestion_universidad";
+        String url = "jdbc:mysql://51.178.152.223:3306/nueva_gestion_universidad";
         return DriverManager.getConnection (url, "Dam4", "ProyectoGrupo4");
     }
 }
